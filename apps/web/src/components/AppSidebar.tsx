e236e11d269a4bc799ca9649c9695726dc19cc88
piira <sekiboh@gmail.com>
2026-09-06 23:13:15 +0300

    Use evexia PNG logo

--- apps/web/src/components/AppSidebar.tsx
+++ apps/web/src/components/AppSidebar.tsx
@@ -42,7 +42,7 @@ import { type FeatureFlag, featureFlags } from "@/lib/featureFlags"
 import { cn } from "@/lib/utils"
 import { useTenantStore } from "@/store/slices/tenantSlice"
 
-const PROJECT_LOGO = "/evexi%CC%81a.svg"
+const PROJECT_LOGO = "/evex%C3%ADa.png"
 
 type NavItem = {
   to: string
